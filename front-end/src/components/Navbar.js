0d920ba079179d50273d4fdaea7d86811a59772c
Samuel Feye <samfeye@gmail.com>
2020-10-25 19:41:55 -0500

    Basic setup for testing on home page

--- front-end/src/components/Navbar.js
+++ front-end/src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import '../styles/NavBar.css';
+import '../styles/Navbar.css'
 import { FaBars, FaTimes } from 'react-icons/fa';
 import { IconContext } from 'react-icons/lib';
 
