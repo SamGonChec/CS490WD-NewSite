6da25b71a164c57664d59b19246f2dd2c4d3a682
Samuel Gonzalez Checa <samuel.gonzalezcheca@outlook.com>
2020-11-01 23:41:21 -0600

    Add Button Files

--- front-end/src/components/Button.js
+++ front-end/src/components/Button.js
@@ -1,17 +1,22 @@
 import React from 'react';
-import './Button.css'
+import '../styles/Button.css'
 import {Link} from 'react-router-dom';
 
 const STYLES = ['button--primary', 'button--secondary'];
 const SIZES = ['button--small', 'button--medium', 'button--large'];
 
-export const Button = ({children, type, onClick, styleButton, sizeButton})
-=>{
+export const Button = ({
+    children, 
+    type, 
+    onClick, 
+    styleButton, 
+    sizeButton
+})  =>  {
     const checkButtonStyle = STYLES.includes(styleButton) ? styleButton : STYLES[0];
-    const checkButtonSize = SIZES.includes(sizeButton) ? sizeButton : STYLE[0];
+    const checkButtonSize = SIZES.includes(sizeButton) ? sizeButton : SIZES[0];
 
     return(
-        <Link to={path} className='btn-mobile'>
+        <Link to='/About' className='btn-mobile'>
             <button className={`btn ${checkButtonStyle} ${checkButtonSize}`} onClick={onClick} type={type}>
                 {children}
             </button>
