c0d35c624bf7956ab32d163223a8144c05192fc1
Samuel Gonzalez Checa <samuel.gonzalezcheca@outlook.com>
2020-12-02 16:42:51 -0600

    Contact page set up

---
+++ front-end/src/pages/Contact.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../styles/App.css';
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
+import Card from '../components/Card';
+
+function Contact() {
+    return (
+        <>
+        <Navbar></Navbar>
+        <p>HISSS</p>
+        <Footer></Footer>
+        </>
+    )
+}
+
+export default Contact;
