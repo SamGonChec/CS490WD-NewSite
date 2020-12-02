c198fb7929e2e53054478c501ca524dd77e760e7
Samuel Gonzalez Checa <samuel.gonzalezcheca@outlook.com>
2020-12-02 16:41:36 -0600

    Services page set up

---
+++ front-end/src/pages/Services.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../styles/App.css';
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
+import Card from '../components/Card';
+
+function Services() {
+    return (
+        <>
+        
+        </>
+    )
+}
+
+export default Services;
